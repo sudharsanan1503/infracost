bab6a59c14333b1abe5bb7a95ca9845840ebae43
sudharsanan1503 <sudharsanan1503@gmail.com>
2026-02-10 19:10:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,28 +3,43 @@ provider "aws" {
 }
 
 locals {
-  tags = {
+  common_tags = {
     Project     = "Infracost-Demo"
     Environment = "test"
     Owner       = "Sudharsanan"
+    ManagedBy   = "Terraform"
   }
 }
 
-resource "aws_instance" "demo" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t3.micro"
+resource "aws_instance" "demo_ec2" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t3.micro"   # BASELINE (cheap)
 
-  tags = local.tags
+  tags = merge(local.common_tags, {
+    Name = "demo-ec2"
+  })
 }
 
-resource "aws_s3_bucket" "app" {
+resource "aws_s3_bucket" "app_bucket" {
   bucket        = "infracost-demo-app-sudharsanan"
   force_destroy = true
-  tags          = local.tags
+  tags          = local.common_tags
 }
 
-resource "aws_cloudwatch_log_group" "logs" {
-  name              = "/infracost/demo"
+resource "aws_s3_bucket" "log_bucket" {
+  bucket        = "infracost-demo-logs-sudharsanan"
+  force_destroy = true
+  tags          = local.common_tags
+}
+
+resource "aws_s3_bucket_logging" "app_bucket_logging" {
+  bucket        = aws_s3_bucket.app_bucket.id
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "access-logs/"
+}
+
+resource "aws_cloudwatch_log_group" "demo_logs" {
+  name              = "/infracost/demo/app"
   retention_in_days = 7
-  tags              = local.tags
+  tags              = local.common_tags
 }
