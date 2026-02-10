6f8db5c1cbd35bb45f174a4a927f9f28d5591729
sudharsanan1503 <sudharsanan1503@gmail.com>
2026-02-10 18:15:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "test_server" {
+resource "aws_instance" "cost-test" {
   ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t3.2xlarge"
+  instance_type = "t3.large"
 
   tags = {
     Environment = "Dev"
