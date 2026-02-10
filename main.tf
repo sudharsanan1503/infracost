ac82731facd7e839c29afe3b10e97eee66eaa9dc
sudharsanan <sudharsanan.r@gmail.com>
2026-02-10 18:44:42 +0530

    Use t3.micro (cheaper instance)

--- main.tf
+++ main.tf
@@ -2,9 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "cost-test" {
+resource "aws_instance" "cost_test" {
   ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t3.micro"   # cheaper than t3.2xlarge
+  instance_type = "t3.micro"   # CHEAP
+
   tags = {
     Environment = "Dev"
     Project     = "Infracost-Test"
